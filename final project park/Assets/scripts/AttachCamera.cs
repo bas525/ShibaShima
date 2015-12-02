870c40fb8b5f9bc7c02b657af19ef2fac051de29
apple <apple@172-16-31-143.DYNAPOOL.NYU.EDU>
2015-12-02 16:40:02 -0500

    FIXED BARREL

--- final project park/Assets/scripts/AttachCamera.cs
+++ final project park/Assets/scripts/AttachCamera.cs
@@ -5,19 +5,27 @@ public class AttachCamera : MonoBehaviour
 {
 	Transform myTransform;
 	public Transform target;
-	public Vector3 offset = new Vector3(0, 10, -10);
+	public Vector3 offset = new Vector3(5, 10, -10);
+	Camera camera;
+
 	
 	void Start()
 	{
 		myTransform = this.transform;
 	}
 	
-	void FixedUpdate()
+	void Update()
 	{
 		if (target != null)
 		{
 			myTransform.position = target.position + offset;
 			myTransform.LookAt(target.position, Vector3.up);
+			Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+			RaycastHit hit;
 		}
+
 	}
+
+
+
 }
