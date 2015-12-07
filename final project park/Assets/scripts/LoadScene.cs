724e75467933d5e53490da9d03f00bf9710feeb4
apple <apple@172-16-204-2.DYNAPOOL.NYU.EDU>
2015-12-06 21:43:30 -0500

    start menu

---
+++ final project park/Assets/scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+	public GameObject loadingImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadNewScene (string _SceneName)
+
+	{
+		loadingImage.SetActive(true);
+		Application.LoadLevel(_SceneName);
+
+	}
+}
